c533af83ff8c1c0fe7c3a13c2ff6a825381095ee
Shariq <shariqsd2003@gmail.com>
2024-07-15 00:07:09 +0530

    final touch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Filter from './components/Filter'
+import Cards from './components/Cards'
+import Spinner from './components/Spinner'
+import { apiUrl, filterData } from './data'
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 function App() {
+  const [courses,setCourses]=useState({});
+  const [category,setCategory]=useState(filterData[0].title);
+  const [loading,setLoading]=useState(false);
+
+  async function fetchCourses(){
+    setLoading(true);
+    try {
+      let res= await fetch(apiUrl);
+      let resData=await res.json();
+      setCourses(resData.data);
+    } catch (error) {
+      toast.error("Error in fetching Courses", { theme: 'dark' });
+    }
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchCourses();
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className='heading-container'>
+          <h1 className='heading'>Top Courses</h1>
+        </div>
+        {/* <div className='filter-container'> */}
+          <Filter filterData={filterData} category={category} setCategory={setCategory}/>
+        {/* </div> */}
+        <div className='cards-container'>
+          {
+            loading ? (<Spinner/>): (<Cards courses={courses} category={category}/>)
+          }
+        </div>
+    </>
   );
 }
 
